002e43d8b6f2333f58b966a59db719c6e2fba9f4
Katie Atkinson <kaitlin@atkinson.mn>
2016-02-27 21:35:42 -0800

    Skeleton file for llist.go

---
+++ llist/llist.go
@@ -0,0 +1,26 @@
+/*
+Provides an interface and class for making linked lists.
+*/
+package llist
+
+import "github.com/KTAtkinson/linked"
+
+type List struct {
+	head linked.Noder
+	tail linked.Noder
+}
+
+// Append adds the given node as the tail of the list.
+func (l *List) Append(n linked.Noder) (err error) {
+	return err
+}
+
+// Push adds the given node as the head of the list.
+func (l *List) Push(n linked.Noder) (err error) {
+	return err
+}
+
+// Pop returns the node at the head of the list.
+func (l *List) Pop() (n linked.Noder, err error) {
+	return n, err
+}
