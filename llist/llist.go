b76ee3587b30b31f225eca1cb01f3cee1ff8209b
Katie Atkinson <kaitlin@atkinson.mn>
2016-02-28 10:40:06 -0800

    Fixed backward logic and testing error messages.

--- llist/llist.go
+++ llist/llist.go
@@ -6,7 +6,6 @@ package llist
 import (
 	"github.com/KTAtkinson/linked"
 )
-
 type List struct {
 	head linked.Noder
 	tail linked.Noder
@@ -14,19 +13,21 @@ type List struct {
 
 // Append adds the given node as the tail of the list.
 func (l *List) Append(n linked.Noder) (err error) {
-	if l.head != nil {
-		l.head.SetPrev(n)
-		n.SetNext(l.head)
-	} else {
-		l.tail = n
-	}
+	if l.head == nil {
+        l.head = n
+	} 
+    if l.tail != nil {
+        n.SetPrev(l.tail)
+        l.tail.SetNext(n)
+    }
 
-	l.head = n
+	l.tail = n
 	return err
 }
 
 // Push adds the given node as the head of the list.
 func (l *List) Push(n linked.Noder) (err error) {
+    
 	return err
 }
 
--- llist/llist_test.go
+++ llist/llist_test.go
@@ -23,12 +23,13 @@ func generateLinks(numNodes int) (head *node.Dnode, tail *node.Dnode) {
 	if numNodes > 0 {
 		head = new(node.Dnode)
 		head.SetData(0)
+        tail = head
 	}
 
 	lastNode := head
 	for i := 1; i < numNodes-1; i++ {
 		newNode := new(node.Dnode)
-		newNode.SetData(0)
+		newNode.SetData(i)
 		newNode.SetPrev(lastNode)
 		lastNode.SetNext(newNode)
 
@@ -37,7 +38,7 @@ func generateLinks(numNodes int) (head *node.Dnode, tail *node.Dnode) {
 
 	if numNodes > 1 {
 		tail = new(node.Dnode)
-		tail.SetData(0)
+		tail.SetData(numNodes-1)
 		lastNode.SetNext(tail)
 		tail.SetPrev(lastNode)
 	}
@@ -57,25 +58,28 @@ func TestAppend(t *testing.T) {
 			linkedList.tail = tail
 		}
 		newNode := new(node.Dnode)
-		newNode.SetData(0)
+		newNode.SetData(c.numNodes+1)
 
 		linkedList.Append(newNode)
-		if linkedList.head != newNode {
-			t.Errorf("Expected new node to be the head of the list when lists length was %#v.", c.numNodes)
+		if linkedList.tail != newNode {
+			t.Errorf("Expected new node to be the tail of the list when lists length was %#v.", c.numNodes)
 		}
 
-		if head != nil {
-			nextToNew, _, _ := newNode.Next()
-			if nextToNew != head {
-				t.Errorf("New head was not linked to list, when list length was %#v.",
-					c.numNodes)
+		if tail != nil {
+			if prev, _, _ := newNode.Prev(); prev != tail {
+				t.Errorf("Exected %#v to preceed the new node, found %#v.",
+                         tail, prev)
 			}
 
-			prevToHead, _, _ := head.Prev()
-			if prevToHead != newNode {
-				t.Error("Old head does not referance the new node.")
+			if next, _, _ := tail.Next(); next != newNode {
+				t.Errorf("Found %#v next to tail, expected %#v.", next, newNode)
 			}
-		}
+		} else {
+            if linkedList.head != newNode {
+                t.Errorf("When list was empty expected head to be %#v, found %#v",
+                         newNode, linkedList.head)
+            }
+        }
 	}
 }
 
@@ -88,15 +92,15 @@ func TestPush(t *testing.T) {
 		linkedList := &List{head: head, tail: tail}
 
 		linkedList.Push(newNode)
-		if linkedList.tail != newNode {
+		if linkedList.head != newNode {
 			t.Error("The tail of the list was not changed.")
 		}
-		if tail != nil {
-			if next, _, _ := tail.Next(); next != newNode {
+		if head != nil {
+			if next, _, _ := newNode.Next(); next != head {
 				t.Error("The old tail does not refer to the new node.")
 			}
-			if prev, _, _ := newNode.Prev(); prev != tail {
-				t.Error("The new node does not refer to the tail.")
+			if prev, _, _ := head.Prev(); prev != newNode {
+		    	t.Error("The new node does not refer to the tail.")
 			}
 		}
 	}
